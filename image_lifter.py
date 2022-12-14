ffdb462668fffe60e20ba19dfe742f31b39a2084
Ryan Sadwick <rsadwick@gmail.com>
2022-12-14 17:01:00 -0500

    Added webp support, using urllib3 instead of requests.

--- image_lifter.py
+++ image_lifter.py
@@ -1,29 +1,29 @@
 import re
 import os
-import requests
+import urllib.request
 from bs4 import BeautifulSoup
 from pathlib import Path
 
+host = 'https://www.gutenberg.org/cache/epub/67098'
 html_request = 'https://www.gutenberg.org/cache/epub/67098/pg67098-images.html'
-response = requests.get(html_request)
+response = urllib.request.urlopen(html_request)
 
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response, 'html.parser')
 images = soup.find_all('img')
 
 urls = [img['src'] for img in images]
 images_path = Path(__file__).parent.resolve() / 'images'
 
 for url in urls:
-    filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+    filename = re.search(r'/([\w_-]+[.](jpg|gif|png|webp))$', url)
 
     if not filename:
         print("Regex didn't match with the url: {}".format(url))
         continue
 
-    with open(os.path.join(images_path, filename.group(1)), 'wb') as file_io:
-        # If the url is relative, make it absolute by joining the base url
-        if 'https' not in url or 'http' not in url:
-            url = '{}{}'.format(html_request, url)
+    if 'https' not in url or 'http' not in url:
+        url = '{}/{}'.format(host, filename.string)
 
-        response = requests.get(url)
-        file_io.write(response.content)
+    with urllib.request.urlopen(url) as url_image:
+        with open(os.path.join(images_path, filename.group(1)), 'wb') as file_io:
+            file_io.write(url_image.read())
